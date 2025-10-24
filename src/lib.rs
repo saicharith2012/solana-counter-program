f64fed321105c9bc806303c0800b5896b528443b
saicharith2012 <saicharithpalabindhela@gmail.com>
2025-10-24 07:19:50 +0530

    updating smart contract

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
+entrypoint!(counter_contract);
+
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct Counter {
     count: u32,
@@ -18,33 +20,28 @@ pub enum InstructionType {
     Decrement(u32),
 }
 
-entrypoint!(counter_contract);
-
 pub fn counter_contract(
     _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     let account = next_account_info(&mut accounts.iter())?;
+    let mut counter = Counter::try_from_slice(&mut account.data.borrow())?;
 
-    let mut counter_data = Counter::try_from_slice(&account.data.borrow())?;
-
-    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
-
-    match instruction_type {
+    match InstructionType::try_from_slice(&instruction_data)? {
         InstructionType::Increment(value) => {
-            msg!("Incrementing counter...");
-            counter_data.count += value;
+            msg!("Incrementing counter");
+            counter.count += value;
         }
-
         InstructionType::Decrement(value) => {
-            msg!("Decrementing counter...");
-            counter_data.count -= value;
+            msg!("Decrementing counter");
+            counter.count -= value;
         }
     }
 
-    let _ = counter_data.serialize(&mut *account.data.borrow_mut());
+    let _ = counter.serialize(&mut *account.data.borrow_mut());
 
-    msg!("Counter successfully update to {}", counter_data.count);
+    msg!("Counter updated to {}", counter.count);
+
     Ok(())
 }
